dffc3108ab9e37b076474ea10f8a7d56ad7703b2
Tharun Atithya <tharunaadhi6@gmail.com>
2024-08-15 11:09:19 +0530

    last

--- Algoritmic Toolbox/Module 4/binary search.py
+++ Algoritmic Toolbox/Module 4/binary search.py
@@ -1,6 +1,14 @@
-def binary_search(x, l, r, find):
-    if l > r:
-        return l -1
-
-a = [1, 2, 3, 4, 5, 6, 7, 8]
-print(binary_search(sorted(a), 7))
+def binary_search(a,key,start=0):
+    if len(a) == 0:
+        return -1
+    mid = len(a)//2
+    if a[mid] == key:
+        return start+mid
+    elif key > a[mid]:
+        return binary_search(a[mid+1:], key, start+mid+1)
+    else:
+        return binary_search(a[:mid], key, start)
+a = sorted(["tharun", "harish", "aqeel", "berbin"])
+print(a)
+print(f"{'berbin'} found in index {binary_search(a, 'berbin')}")
+print(20//2)
