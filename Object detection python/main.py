308af79d54eb9f12e9498e4ac770b88d4371e8bf
Tharun Atithya <tharunaadhi6@gmail.com>
2024-10-01 10:33:13 +0530

    object detection initial commit

--- Object detection python/main.py
+++ Object detection python/main.py
@@ -1,2 +1,6 @@
-import ultralytics
-print(ultralytics.checks())
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('Yolo models/yolov8l.pt')
+result = model('coconut image(3).jpg', show=True)
+cv2.waitKey(0)
